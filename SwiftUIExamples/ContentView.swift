1b003162fb76f613a8dd01de6bf7f601c343447b
GaneshRajuGalla <ganeshrajugalla@gmail.com>
2024-11-04 00:02:11 +0530

    transitions bootcamp

---
+++ SwiftUIExamples/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIExamples
+//
+//  Created by Ganesh Raju Galla on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+        
+    // MARK: - Body
+    var body: some View {
+        TransitionsBootCamp()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIExamples/SwiftUIExamplesApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIExamplesApp.swift
+//  SwiftUIExamples
+//
+//  Created by Ganesh Raju Galla on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
